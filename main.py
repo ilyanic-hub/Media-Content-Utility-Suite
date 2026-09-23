f119ce0c39cb349d485667bc417730f3d1391986
ilyanic-hub <ilyanic20@gmail.com>
2026-09-23 12:33:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def get_sitemap():
 
 @app.get("/telderia98d4f8dab4483a155a5cfc0f6faff60.txt")  # имя файла от Telderi
 async def telderi_verify():
-    return FileResponse("telderi_a98d4f8d.html")
+    return FileResponse("telderia98d4f8dab4483a155a5cfc0f6faff60.txt")
 
 # ПАРСИНГ И СКАЧИВАНИЕ YOUTUBE
 # ---------------------------------------------------------
